b6497bca679936b26df25bce96f95d06627e7208
jacksontaylorjp <jacksontaylorjp@gmail.com>
2023-07-16 17:45:01 -0300

    ajustes no readme

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# portfolio
-Meu portfolio.
+# Portfólio
+<p align="left">Projeto construído com o objetivo de mostrar meus dados e experiencias profissionais.</p>
+<p align="center"> 
+    <img src="./assets/img/portfolio.gif">
+</p>
+
--- index.js
+++ index.js
@@ -1,26 +1,23 @@
 const camposDoFormulario = document.querySelectorAll("[required]");
 const formulario = document.querySelector("[data-formulario]");
+let mensagem = "";
 
 formulario.addEventListener("submit", (e) => {
-    e.preventDefault();
+        const listaRespostas = {
+            "nome": e.target.elements["nome"].value,
+            "email": e.target.elements["email"].value,
+            "assunto": e.target.elements["assunto"].value,
+            "mensagem": e.target.elements["mensagem"].value,
+        }
 
-    const listaRespostas = {
-        "nome": e.target.elements["nome"].value,
-        "email": e.target.elements["email"].value,
-        "assunto": e.target.elements["assunto"].value,
-        "mensagem": e.target.elements["mensagem"].value,
-    }
-
-    localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
-
-    // window.location.href = "./abrir-conta-form-2.html";
+        localStorage.setItem("cadastro", JSON.stringify(listaRespostas));
+        alert("Mensagem enviada com sucesso!")
 });
 
 camposDoFormulario.forEach((campo) => {
     campo.addEventListener("blur", () => verificaCampo(campo));
     campo.addEventListener("invalid", evento => evento.preventDefault());
 });
-
 const tiposDeErro = [
     'valueMissing',
     'typeMismatch',
@@ -29,30 +26,31 @@ const tiposDeErro = [
     'tooLong',
     'customError'
 ]
-
 const mensagens = {
     nome: {
         valueMissing: "O campo de nome não pode estar vazio.",
         patternMismatch: "Por favor, preencha um nome válido.",
-        tooShort: "Nome muito curto. Por favor, preencha um nome válido.",
+        tooShort: "Nome muito curto. Digite no mínimo 3 caracteres.",
     },
     email: {
         valueMissing: "O campo de e-mail não pode estar vazio.",
-        typeMismatch: "Por favor, preencha um email válido.",
-        tooShort: "Email muito curto. Por favor, preencha um email válido."
+        typeMismatch: "Por favor, preencha um email válido. Ex: nome@exemplo.com",
+        tooShort: "Email muito curto. Digite no mínimo 4 caracteres."
     },
     assunto: {
         valueMissing: "O campo de assunto não pode estar vazio.",
-        tooShort: "Assunto muito curto. Por favor, preencha um assunto válido.",
+        tooShort: "Assunto muito curto. Digite no mínimo 5 caracteres.",
     },
     mensagem: {
         valueMissing: "O campo de mensagem não pode estar vazio.",
-        tooShort: "Mensagem muito curto. Por favor, preencha um mensagem válida.",
+        tooShort: "Mensagem muito curta.  Digite no mínimo 50 caracteres.",
+    },
+    btEnviar: {
+        customError: "Para enviar é necessário preencher todos os campos do formulário"
     }
 }
 
 function verificaCampo (campo){
-    let mensagem = "";
     campo.setCustomValidity('');
     tiposDeErro.forEach(erro => {
         if (campo.validity[erro]) {
@@ -65,5 +63,7 @@ function verificaCampo (campo){
         mensagemErro.textContent = mensagem;
     } else {
         mensagemErro.textContent = "";
+        document.querySelector(".contato__botao").removeAttribute("disabled");
     }
 }
+
